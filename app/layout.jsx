89390527aae9f8cdf21b3bae311fbc4f6871aaaf
hirenmaddara <hiren.maddara@gmail.com>
2025-08-01 00:35:36 +0200

    config tw in global.css phew

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,15 +1,11 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const jetBrainsMono = JetBrains_Mono({
+  subsets: ["latin"], 
+  weight: ["100", '200', '300','400', '500', '600', '700', '800'],
+  variable: '--font-jetbrainsMono'
+})
 
 export const metadata = {
   title: "Create Next App",
@@ -20,7 +16,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${jetBrainsMono.variable} antialiased`}
       >
         {children}
       </body>
