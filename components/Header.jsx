07160cb1e4a34cd0bc16ef6e2db8cadb9bd889bb
hirenmaddara <hiren.maddara@gmail.com>
2025-08-01 12:54:12 +0200

    finished Header, editted sheet and button ui components

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,6 +1,7 @@
 import Link from "next/link"
 import { Button } from "./ui/button"
 import Nav from "./Nav"
+import MobileNav from "./MobileNav"
 
 const Header = () => {
   return <header className="py-8 xl:py-12 text-white bg-pink-50/20">
@@ -22,7 +23,7 @@ const Header = () => {
 
         {/* mobile nav */}
         <div className="xl:hidden">
-            mobile nav
+            <MobileNav />
         </div>
         
     </div>    
