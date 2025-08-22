869a81dc67b9846ba35b40864d17f1ac03787b15
hirenmaddara <hiren.maddara@gmail.com>
2025-08-23 00:02:01 +0200

    maintainance msg overall

--- app/layout.jsx
+++ app/layout.jsx
@@ -13,10 +13,25 @@ export const metadata = {
   description: "Hiren Maddara Portfolio",
 };
 
-export default function RootLayout({ children }) {
-  return (
+export default function RootLayout({}) {
+	return (
 		<html lang="en" className={`${jetBrainsMono.variable} antialiased`}>
-			<body className="">{children}</body>
+			<body className="">
+				<main className="h-screen text-center flex flex-col items-center justify-center">
+					<img
+						src="https://www.svgrepo.com/show/426192/cogs-settings.svg"
+						alt="Logo"
+						className="mb-8 h-40"
+					/>
+					<h1 className="text-xl md:text-5xl lg:text-6xl font-bold text-center text-gray-700 dark:text-white mb-4">
+						Site is under maintenance
+					</h1>
+					<p className="text-center text-gray-500 dark:text-gray-300 text-lg mb-8">
+						Working to stay upto date with designs and all. See you when I'm
+						back!
+					</p>
+				</main>
+			</body>
 		</html>
 	);
 }
